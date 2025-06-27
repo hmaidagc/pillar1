86a42a90ea947885680ea47d8052eb51a2a57d93
hmaidagc <hmaidagc@gmail.com>
2025-06-27 15:29:40 +0400

    Delete src directory

--- src/app/editor/[projectId]/page.tsx
+++
@@ -1,8 +0,0 @@
-import { Editor } from '@/app/features/editor/components/editor'
-import React from 'react'
-
-const EditorProjectIdPage = () => {
-  return <Editor />
-}
-
-export default EditorProjectIdPage
--- src/app/features/editor/components/toolbar.tsx
+++
@@ -1,5 +0,0 @@
-'use client'
-
-export const Toolbar = () => {
-  return <div></div>
-}
